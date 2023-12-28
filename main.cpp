908862408a438d2bda06610b00462ad2759ca31a
GalichGNDR <galichgndr2@gmail.com>
2023-12-28 08:04:19 +0300

    Ввод a,b

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "sdt.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a,b;
+
+    cout << "Введите a"<< endl;
+    cin >> a;
+    cout << "Введите b"<< endl;
+    cin >> b;
+    
+}
